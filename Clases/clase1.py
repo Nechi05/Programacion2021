d3f2d13c2fbcdf6a61bb42574cd7428942044e2e
Nechi05 <iriarte.abraham@uces.edu.co>
2021-02-09 07:46:41 -0500

    clase holamundo

---
+++ Clases/clase1.py
@@ -0,0 +1,8 @@
+saludo = "Hola Mundo"
+print(saludo)
+print("Hablame menor")
+print("q´hubo bb donde estas acabo de aterrizar en Medellín")
+nombre = "Abraham"
+edad = "20"
+estatura = "1.84"
+print(nombre,edad,estatura)
