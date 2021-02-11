2db20eac33c7c73a5658815adf866bc5d7249cc2
Nechi05 <iriarte.abraham@uces.edu.co>
2021-02-11 14:27:44 -0500

    subiendo taller de booleans con operaciones

---
+++ Talleres/Booleans1.py
@@ -0,0 +1,15 @@
+# practicando las operaciones matemáticas con python 
+# se asignan dos numeros para proceder a realizar diversas operaciones con ellos
+numeroA = 136
+numeroB = 47
+suma = numeroA + numeroB
+print(f"la suma de estos dos numeros dió {suma} de forma exitosa")
+resta = numeroA - numeroB
+print(f"la resta de estos dos numeros dió {resta}")
+multiplica = numeroA * numeroB
+print(f"la multiplicación de A x B dió {multiplica} correctamente")
+divide = numeroA/numeroB
+print(f"la división de A x B dió {divide} correctamente")
+exponente = numeroA ** numeroB
+print(f"el exponente entre numero A y B es {exponente}")
+# recordar que las comillas deben encerrar toda la frase hasta antes del parentesis 
