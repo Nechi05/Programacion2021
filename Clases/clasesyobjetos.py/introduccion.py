c343b9aa0f88c2f8eab3347765d246d0670da716
Nechi05 <iriarte.abraham@uces.edu.co>
2021-04-15 08:22:17 -0500

    anexo abogado e ingeniero

--- Clases/clasesyobjetos.py/introduccion.py
+++ Clases/clasesyobjetos.py/introduccion.py
@@ -49,9 +49,24 @@ class Humano ():
             ingresarMontos = input (preguntaIngresarMontos)
         return self.dinero 
 
-class Biomedico ( Humano ):
+class Ingeniero(Humano):
+    def __init__(self,nombreEntrada, edadEntrada,estaturaEntrada,areaEntrada):
+        Humano.__init__(self, nombreEntrada,edadEntrada,estaturaEntrada)
+        self.area = areaEntrada
+    def solucionarProblemas(self, problema):
+        print(f'Hola soy un ingeniero y me llamo {self.nombre} y procedo a solucionar el problema {problema}')
+class Programador (Humano):
+    def crearAlgoritmo(self, algoritmo):
+        print(f'Hola soy {self.nombre} y procedo a resolver el algoritmo {algoritmo}')
+
+class Biomedico (Ingeniero,Programador):
     def mantenimientoEquiposMedicos (self, nombreEquipo):
         print (f'Hola soy {self.nombre} y procedo a arreglar el {nombreEquipo}')
+
+class Abogado(Humano):
+    def levantarAccionDeTutela(self,nombreCliente):
+        print(f'Hola soy {self.nombre} y estoy representando a {nombreCliente}')
+
 
 
 humano1 = Humano('Abraham',21,1.84)
@@ -67,6 +82,13 @@ humano1.recorrerDistancia (30)
 humano2.mostrarAtributos
 totalAhorrado = humano2.ahorraDinero()
 humano2.mostrarAtributos ()
-biomedico = Biomedico ('Duvan',20,1.85)
+biomedico = Biomedico ('Duvan',20,1.85,'BioEstadística')
 biomedico.mostrarAtributos ()
 biomedico.mantenimientoEquiposMedicos ('Electrocardiograma')
+
+abogado1 = Abogado('Stiven',34,1.94)
+abogado1.mostrarAtributos()
+abogado1.levantarAccionDeTutela(biomedico.nombre)
+biomedico.crearAlgoritmo('Fibonacci')
+biomedico.solucionarProblemas('Ocupación alta de UCIs')
+print(biomedico.area)
