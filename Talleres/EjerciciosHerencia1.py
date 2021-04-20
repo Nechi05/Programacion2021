fb3f4660c2e5d5f5a5944111680d7fba4d5bbb2d
Nechi05 <iriarte.abraham@uces.edu.co>
2021-04-20 11:02:33 -0500

    ..

--- Talleres/EjerciciosHerencia1.py
+++ Talleres/EjerciciosHerencia1.py
@@ -13,7 +13,6 @@ class Persona ():
 def linedesign(cantidad = 10, simbolo = '#'):
         print (simbolo *cantidad)
         return None
-
 #....Heredar clase Persona para volverla Doctor, Nutricionista y Abogado....#
 class Doctor (Persona):
     def __init__(self,idEntrada,nombreEntrada, edadEntrada,especialidadEntrada):
@@ -26,37 +25,16 @@ class Doctor (Persona):
         ''')
 
 #.......Punto3......#
-PREGUNTA_PESO = "Cuanto pesas en kg? : "
-PREGUNTA_ESTATURA = "Cuanto mides en mts? : "
-MENSAJE_RESULTADO = "El resultado de la prueba de obesidad es..."
-MENSAJE_BAJO_PESO = "Estas bajo de peso"
-MENSAJE_ADECUADO = "Estás dentro de tu peso ideal"
-MENSAJE_SOBREPESO = "Ten cuidado, estas en sobrepeso"
-MENSAJE_OBESO = "Cuidado! estás obeso, tu salud corre riesgo"
 class Nutricionista (Persona):
     def _init_ (self,idEntrada,nombreEntrada,edadEntrada,universidadEntrada):
         Persona.__init__(self,idEntrada,nombreEntrada,edadEntrada)
         self.universidad = universidadEntrada
-    def calcularimc (self,resultado):
-        peso = float(input(PREGUNTA_PESO))
-        estatura = float(input(PREGUNTA_ESTATURA))
-        imc = peso/(estatura**2)
-        isBajoPeso = imc < 18.5
-        isAdecuado = imc >= 18.5 and imc < 25
-        isSobrepeso = imc >= 25 and imc < 30
-        resultado = ""
-        if(isBajoPeso):
-            resultado=(MENSAJE_BAJO_PESO)
-        elif(isAdecuado):
-            resultado=(MENSAJE_ADECUADO)
-        elif(isSobrepeso):
-            resultado=(MENSAJE_SOBREPESO)
-        else:
-            resultado=(MENSAJE_OBESO)
-        print (f'''Hola, soy tu nutricionista {self.nombre}
-                egresada de la universidad {self.universidad}
-                tu imc es de {resultado}
-                ''')
+    def calcularimc (self,alturaEntrada,pesoEntrada):
+        pesoIMC = int (input ('¿Cuál es tu peso actual?'))
+        alturaIMC = float (input('¿Cuanto mides en metros?'))
+        print ('su imc es de ' ,pesoIMC/(alturaIMC**2))
+    def presentarse (self, mensaje1):
+        print (f'soy {self.nombre}, tu nutricionista y hoy voy a calcular tu IMC, pero primero debes darme unos datos')
 #............Punto4..........#
 class Abogado (Persona):
     def __init__(self,idEntrada,nombreEntrada, edadEntrada,especialidadDerecho,universidadAtributo):
@@ -74,7 +52,11 @@ linedesign (3, '.......')
 doctor1 = Doctor (1001548900,'Jhonatan',35,'Cardiólogo')
 doctor1.tratarEnfermedad ('Miocardiopatía')
 linedesign (3, '.......')
-#error nutri
+nutricionista1 = Nutricionista (123456,'Juan',28)
+nutricionista1.presentarse ('.')
+nutricionista1.calcularimc (1,2)
 linedesign (3, '.......')
 abogado1 = Abogado (1001548389,'Julian',47,'Derecho Penal','Convenios con Bufetes Internacionales')
 abogado1.representarCliente ('Mauro','Lavado de activos')
+
+
